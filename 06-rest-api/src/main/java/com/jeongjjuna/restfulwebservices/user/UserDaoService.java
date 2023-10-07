99b64aa7e90054ae6141efb60d18480492badb81
jeongjihun <wlgns2234@naver.com>
2023-10-08 08:48:10 +0900

    start REST API

---
+++ 06-rest-api/src/main/java/com/jeongjjuna/restfulwebservices/user/UserDaoService.java
@@ -0,0 +1,43 @@
+package com.jeongjjuna.restfulwebservices.user;
+
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+@Component
+public class UserDaoService {
+
+    private static List<User> users = new ArrayList<>();
+
+    private static int usersCount = 3;
+
+
+    static {
+        users.add(new User(1, "a", LocalDate.now().minusYears(25)));
+        users.add(new User(2, "b", LocalDate.now().minusYears(30)));
+        users.add(new User(3, "c", LocalDate.now().minusYears(32)));
+    }
+
+    public List<User> findAll() {
+        return users;
+    }
+
+    public User findOne(int id) {
+        Predicate<? super User> predicate = user -> user.getId().equals(id);
+        return users.stream().filter(predicate).findFirst().orElse(null);
+    }
+
+    public User save(User user) {
+        user.setId(++usersCount);
+        users.add(user);
+        return user;
+    }
+
+    public void deleteById(int id) {
+        Predicate<? super User> predicate = user -> user.getId().equals(id);
+        users.removeIf(predicate);
+    }
+}
