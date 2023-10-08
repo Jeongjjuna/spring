a32ed6dbc65229579a1ff61f36200c163177e173
jeongjihun <wlgns2234@naver.com>
2023-10-08 21:26:21 +0900

    문서화, 컨텐츠협상, 국제화, 버전관리

---
+++ 06-rest-api/src/main/java/com/jeongjjuna/restfulwebservices/versioning/PersonV2.java
@@ -0,0 +1,14 @@
+package com.jeongjjuna.restfulwebservices.versioning;
+
+public class PersonV2 {
+
+    private Name name;
+
+    public PersonV2(Name name) {
+        this.name = name;
+    }
+
+    public Name getName() {
+        return name;
+    }
+}
